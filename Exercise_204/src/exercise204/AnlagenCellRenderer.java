7da625b4a1bbad57573778e9276399ed1caae2f6
Felix <prefec16@new.edvhtl.at>
2018-10-05 12:25:31 +0200

    added cellrenderer

---
+++ Exercise_204/src/exercise204/AnlagenCellRenderer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise204;
+
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+/**
+ *
+ * @author Felix
+ */
+public class AnlagenCellRenderer implements TableCellRenderer{
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        JLabel label = new JLabel();
+        Anlage anlage = (Anlage) value;
+        
+return label;
+    }
+    
+}
