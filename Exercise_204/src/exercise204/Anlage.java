783c4611ab7e58c3c1a55a1fb62c08a19633bd83
Kilian <kiliansc5@gmail.com>
2018-10-09 09:09:35 +0200

    Implemented calc method

--- Exercise_204/src/exercise204/Anlage.java
+++ Exercise_204/src/exercise204/Anlage.java
@@ -8,20 +8,29 @@ public class Anlage {
     private final String name;
     private final String year;
     private final String value;
-    private double yearValue;
+    private final int nd;
+    private double[] values;
 
-    public Anlage(String name, String year, String value) {
+    public Anlage(String name, String year, String value,int nd) {
         this.name = name;
         this.year = year;
         this.value = value;
+        this.nd= nd;
     }
     
     public void calc(int year) {
-
+        double init = Double.parseDouble(this.year);
+        double value = Double.parseDouble(this.value);
+        values[0]=init-year;
+        values[1]=value/nd;
+        values[2]=values[1]*values[0];
+        values[3]=value-values[2];
+        values[4]= values[3]-values[1];
+        
     }
 
-    public void setYearValue(double yearValue) {
-        this.yearValue = yearValue;
+    public double[] getValues() {
+        return values;
     }
 
     
